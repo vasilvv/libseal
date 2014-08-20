adab8293d941aa57e3f8f04ba7d84b6ff184a03b
Victor Vasiliev <vasilvv@mit.edu>
2014-08-20 19:57:54 -0400

    Extend ASN.1 parser tests

--- asn1/parser_tests.cc
+++ asn1/parser_tests.cc
@@ -12,12 +12,23 @@ asn1::ParserOptions default_options(asn1::Encoding enc = asn1::DER) {
 }
 
 /**
+ * Repeat a string N times.
+ */
+std::string strrep(std::string str, size_t num_rep) {
+    std::string result(str.length() * num_rep, ' ');
+    for (size_t i = 0; i < result.length(); i++) {
+        result[i] = str[i % str.length()];
+    }
+    return result;
+}
+
+/**
  * Returns if the parsing of given hex-encoded DER blob failed.
  */
-bool does_parsing_fails(const char *hex, bool parse_all = true) {
-    bytestring seq = bytestring::from_hex(hex);
+bool does_parsing_fails(std::string hex, bool parse_all = true, bool enforce_der = true) {
+    bytestring seq = bytestring::from_hex(hex.c_str());
 
-    asn1::Parser parser(seq.cmem(), default_options());
+    asn1::Parser parser(seq.cmem(), default_options(enforce_der ? asn1::DER : asn1::BER));
     asn1::Data_u result = parse_all ? parser.parse_all() : parser.parse();
     return result.get() == nullptr;
 }
@@ -98,6 +109,87 @@ TEST(ParserGeneric, ThreeBlobSetOrder) {
 
     ASSERT_NE(nullptr, result_ber.get());
     ASSERT_TRUE(result_ber->is_universal_type(asn1::UTSet));
+}
+
+TEST(ParserGeneric, LongLengthForm) {
+    // Create an octet string which has 256 bytes, repeating "00 01 02 03" as
+    // its contents
+    bytestring longstr = bytestring::from_hex(("04820100" + strrep("00010203", 64)).c_str());
+
+    asn1::Parser parser(longstr.cmem(), default_options());
+    asn1::Data_u result = parser.parse_all();
+
+    ASSERT_NE(nullptr, result.get());
+    ASSERT_TRUE(result->is_universal_type(asn1::UTOctetString));
+
+    const uint8_t *body = result->get_body().cptr();
+    for (size_t i = 0; i < result->get_body().size(); i++) {
+        ASSERT_EQ(i % 4, body[i]);
+    }
+}
+
+TEST(ParserGeneric, ShortestLengthConstraint) {
+    // Use long form of the length where short would have sufficed
+    ASSERT_TRUE(does_parsing_fails("04810100"));
+    ASSERT_FALSE(does_parsing_fails("04810100", true, false));
+
+    // Use a redundant byte in the long form
+    ASSERT_TRUE(does_parsing_fails("048200ff" + strrep("42", 255)));
+    ASSERT_FALSE(does_parsing_fails("048200ff" + strrep("42", 255), true, false));
+
+    // Stress test by using ridiculous length
+    ASSERT_TRUE(does_parsing_fails("048f" + strrep("ff", 0xf)));
+    ASSERT_TRUE(does_parsing_fails("0484" + strrep("ff", 0xf)));
+}
+
+// Basic boolean test, contains only DER-valid bools
+//  0   6: SEQUENCE {
+//  2   1:   BOOLEAN TRUE
+//  5   1:   BOOLEAN FALSE
+//       :   }
+TEST(ParserBoolean, Basic) {
+    bytestring bools = bytestring::from_hex("30060101ff010100");
+
+    asn1::Parser parser(bools.cmem(), default_options());
+    asn1::Data_u result = parser.parse_all();
+
+    ASSERT_NE(nullptr, result.get());
+    ASSERT_TRUE(result->is_universal_type(asn1::UTSequence));
+
+    asn1::ConstructedData* container = static_cast<asn1::ConstructedData*>(result.get());
+    auto &elems = container->get_elements();
+    ASSERT_EQ(2, elems.size());
+    for (size_t i = 0; i < 1; i++) {
+        ASSERT_TRUE(elems[i]->is_universal_type(asn1::UTBoolean));
+        asn1::BooleanData *boolean = static_cast<asn1::BooleanData*>(elems[i].get());
+        ASSERT_TRUE(boolean->get() xor i);
+    }
+}
+
+//  0   1: BOOLEAN TRUE
+//       :   Error: BOOLEAN '01' has non-DER encoding.
+TEST(ParserBoolean, DERConstraint) {
+    ASSERT_TRUE(does_parsing_fails("010101"));
+    ASSERT_FALSE(does_parsing_fails("010101", true, false));
+}
+
+// Test whether OID parser is integrated correctly and does validate
+TEST(ParserOID, GoodOID) {
+    auto oid = asn1::OID({1, 2, 840, 113554, 4, 1, 112411, 1, 12345});
+    bytestring oidder = bytestring::from_hex("060e2a864886f712040186ee1b01e039");
+
+    asn1::Parser parser(oidder.cmem(), default_options());
+    asn1::Data_u result = parser.parse_all();
+
+    ASSERT_NE(nullptr, result.get());
+    ASSERT_TRUE(result->is_universal_type(asn1::UTOID));
+
+    asn1::OIDData *parsed_oid = static_cast<asn1::OIDData*>(result.get());
+    EXPECT_EQ(oid, parsed_oid->get());
+}
+
+TEST(ParserOID, BadOID) {
+    ASSERT_TRUE(does_parsing_fails("0602ffff"));
 }
 
 int main(int argc, char **argv) {
